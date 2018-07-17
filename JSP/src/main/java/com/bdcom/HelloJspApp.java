d07f9e248dc625be1db86e81310a26b168bf1f7e
cjzm888999 <404436080@qq.com>
2018-07-17 14:21:30 +0800

    添加打印路径

--- JSP/src/main/java/com/bdcom/HelloJspApp.java
+++ JSP/src/main/java/com/bdcom/HelloJspApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloJspApp {
     public static void main(String args[]){
         SpringApplication.run(HelloJspApp.class,args );
+        System.out.println(System.getProperty("user.dir"));
 
     }
 }
