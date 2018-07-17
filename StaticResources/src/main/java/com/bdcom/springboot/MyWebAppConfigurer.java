c55d4a9d2e315d63b80379cbd5c50e035e353f2a
cjzm888999 <404436080@qq.com>
2018-07-17 18:56:38 +0800

    资源文件指定配置

---
+++ StaticResources/src/main/java/com/bdcom/springboot/MyWebAppConfigurer.java
@@ -0,0 +1,18 @@
+package com.bdcom.springboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyWebAppConfigurer
+        extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/myres/**").addResourceLocations("classpath:/myres/");
+        super.addResourceHandlers(registry);
+    }
+
+}
