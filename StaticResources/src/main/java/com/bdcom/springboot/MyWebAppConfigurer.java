41a7d624ea31ebfb62e96a55a1101ad06aca1bdf
cjzm888999 <404436080@qq.com>
2018-07-17 19:07:51 +0800

    剔除无用引用

--- StaticResources/src/main/java/com/bdcom/springboot/MyWebAppConfigurer.java
+++ StaticResources/src/main/java/com/bdcom/springboot/MyWebAppConfigurer.java
@@ -1,7 +1,6 @@
 package com.bdcom.springboot;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
