277ae65a730d76aec330e11962c66a20864be049
cjzm888999 <404436080@qq.com>
2018-07-18 18:38:30 +0800

    Druid数据库连接监控

---
+++ Druid/src/main/java/com/bdcom/cn/DruidApp.java
@@ -0,0 +1,14 @@
+package com.bdcom.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class DruidApp {
+    public static void main(String args[]){
+        SpringApplication.run(DruidApp.class,args);
+        //http://127.0.0.1:8080/druid/index.html
+    }
+}
