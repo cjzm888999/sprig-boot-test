a4987a12bd27d501a7d48996a12c0adc6718d3bb
cjzm888999 <404436080@qq.com>
2018-07-18 09:57:50 +0800

    资源测试添加测试项

--- StaticResources/src/main/java/com/bdcom/springboot/MyWebAppConfigurer.java
+++ StaticResources/src/main/java/com/bdcom/springboot/MyWebAppConfigurer.java
@@ -10,7 +10,12 @@ public class MyWebAppConfigurer
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/myres/**").addResourceLocations("classpath:/myres/");
+        // registry.addResourceHandler("/myres/**").addResourceLocations("classpath:/myres/");//http://localhost:8080/myres/P8510.png
+        //registry.addResourceHandler("/my/**").addResourceLocations("classpath:/myres/");//http://localhost:8080/my/P8510.png
+        //registry.addResourceHandler("/**").addResourceLocations("classpath:/myres/").addResourceLocations("classpath:/public/");
+        // (/** 会覆盖系统默认的配置)   http://localhost:8080/P8510.png
+        // 可以直接使用addResourceLocations 指定磁盘绝对路径，同样可以配置多个位置，注意路径写法需要加上file:
+        registry.addResourceHandler("/my/**").addResourceLocations("file:E:/Resources/");//http://localhost:8080/my/Resources.txt
         super.addResourceHandlers(registry);
     }
 
