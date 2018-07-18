0a7daa3787a347d61a7a51ffbbd87a53d49f8354
cjzm888999 <404436080@qq.com>
2018-07-18 19:11:27 +0800

    修正

--- Druid/src/main/java/com/bdcom/cn/DruidApp.java
+++ Druid/src/main/java/com/bdcom/cn/DruidApp.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
+//@ServletComponentScan
 public class DruidApp {
     public static void main(String args[]){
         SpringApplication.run(DruidApp.class,args);
         //http://127.0.0.1:8080/druid/index.html
+        //http://127.0.0.1:8080/druid2/index.html
     }
 }
