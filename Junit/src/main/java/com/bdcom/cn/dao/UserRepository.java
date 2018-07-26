2fa3ef5fbf9d1719c5066497589f1dc65e453630
cjzm888999 <404436080@qq.com>
2018-07-26 13:16:27 +0800

    事务处理移交给service层

--- Junit/src/main/java/com/bdcom/cn/dao/UserRepository.java
+++ Junit/src/main/java/com/bdcom/cn/dao/UserRepository.java
@@ -13,12 +13,12 @@ public interface UserRepository extends JpaRepository<User,Long> {
     public List<User> findUserByName(String name);
     public User findUserById(int id);
     @Modifying
-    @Transactional
+   // @Transactional
     @Query(value="update User set email=:email,name=:name where id=:id" , nativeQuery=true )
     public int updateUser(@Param("email") String mail,@Param("name") String name, @Param("id") int id);
 
     @Modifying
-    @Transactional
+   // @Transactional
     @Query(value="update User set email=:email where id=:id" , nativeQuery=true )
     public int updateUserEmail(@Param("email") String email, @Param("id") int id);
 }
