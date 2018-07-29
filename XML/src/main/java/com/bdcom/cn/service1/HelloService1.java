0b7b892f5488890bca5df76ee86f2a2ec5a542af
cjzm888999 <404436080@qq.com>
2018-07-29 17:51:51 +0800

    xml加载bean

---
+++ XML/src/main/java/com/bdcom/cn/service1/HelloService1.java
@@ -0,0 +1,10 @@
+package com.bdcom.cn.service1;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService1 {
+    public HelloService1(){
+        System.out.println("HelloService1 初始化完成");
+    }
+}
---
+++ XML/src/main/java/com/bdcom/cn/service2/HelloService2.java
@@ -0,0 +1,10 @@
+package com.bdcom.cn.service2;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService2 {
+    public HelloService2(){
+        System.out.println("HelloService2 初始化完成");
+    }
+}
